6501c829b6d6594c7da0ec713ecfb4c5275ea4bf
DevOnLinux <b.ge.haines@gmail.com>
2014-08-04 23:13:36 -0400

    Added the rest of the stock parameters including bid/ask, beta and volume etc. Wrapped these class value assignements in try blocks to catch AttributeError exceptions which occur if any of the values in the table are N/A.

--- yahooPrice.py
+++ yahooPrice.py
@@ -13,15 +13,52 @@ class YahooPrice():
 	def __init__(self, tkr):
 		url = 'http://finance.yahoo.com/q?s=%s' %tkr
 
-		try:
-			tickerPage = urlopen(url)
-		except Exception, e:
-			raise
+		
+		tickerPage = urlopen(url)
+		
 
 		#Turn the page into soup
 		soup = BeautifulSoup(tickerPage)
 
-		#Find the Last Price value in the soup
+		#Find the real-time Last Price value in the soup
 		for y in soup.find_all("span", class_="time_rtq_ticker"):
 			x = y.contents[0].contents
 		self.LastPrice = x[0]
+
+		#Pull data from price table
+		s = soup.find_all("td", class_="yfnc_tabledata1")
+
+		try:
+			self.prevClose = s[0].contents[0]
+		except AttributeError:
+			self.prevClose = 'N/A'
+
+		try:
+			self.tOpen = s[1].contents[0]
+		except AttributeError:
+			self.tOpen = 'N/A'
+
+		try:
+			self.bid = s[2].contents[0].contents[0]
+		except AttributeError:
+			self.bid = 'N/A'		
+		
+		try:
+			self.ask = s[3].contents[0].contents[0]
+		except AttributeError:
+			self.ask = 'N/A'
+
+		try:
+			self.beta = s[5].contents[0]
+		except AttributeError:
+			self.beta = 'N/A'
+
+		try:	
+			self.volume = s[9].contents[0].contents[0]
+		except AttributeError:
+			self.volume = 'N/A'
+
+		try:
+			self.avgVolume = s[10].contents[0]
+		except AttributeError:
+			self.avgVolume = 'N/A'
