d3dede64288139fbd0f4eba41124ecb23c3c3a9d
DevOnLinux <b.ge.haines@gmail.com>
2014-08-07 17:01:11 -0400

    Major revamp of the client. Reformatted to add main loop. Added a function that takes a character and adds it to the end of the given string while dropping the first character of the given string. This function allows for parsing the streamer replies as they arrive one character at a time in order to identify the pertinent tags.
    A flag is flipped between True and False as the tags come in. While the flag is True, characters are recorded.

--- streamerClient.py
+++ streamerClient.py
@@ -1,15 +1,69 @@
 import requests
+'''This will open a connection to the yahoo streamerapi server and 
+print the data dict to the terminal.'''
 
-#r = requests.get('http://streamerapi.finance.yahoo.com/streamer/1.0?s=TSLA,MSFT&k=l10&callback=parent.yfs_u1f&mktmcb=parent.yfs_mktmcb&gencallback=parent.yfs_gencb', stream=True)
-r = requests.get('http://streamerapi.finance.yahoo.com/streamer/1.0?s=MSFT&k=l10,a00,a50,b00,b60,c63,g53,h53,j10,l84,p20,p43,t53,v53&j=c63,j10,l84,p20,p43,t53&r=0&callback=parent.yfs_u1f&mktmcb=parent.yfs_mktmcb&gencallback=parent.yfs_gencb', stream=True)
-#curl -s -o - -N 'http://streamerapi.finance.yahoo.com/streamer/1.0?s=MSFT&k=l10,a00,a50,b00,b60,c63,g53,h53,j10,l84,p20,p43,t53,v53&j=c63,j10,l84,p20,p43,t53&r=0&callback=parent.yfs_u1f&mktmcb=parent.yfs_mktmcb&gencallback=parent.yfs_gencb'
+def stringGen(Chars,Char,numChars):
+	'''Take in one character at a time and add it to the end of the string
+	Chars while dropping the first character of the Chars string.
+	eg: Chars='hart' Char='s' => 'arts'.'''
 
-tag = ''
-count =0
-for char in r.iter_content():
-	tag = tag + char.decode()
-	count += 1
-	if count > 500:
-		print(tag)
-		tag = ''
-		count = 0
+	charList = []
+	newList=[]
+
+	if numChars==8:
+		if len(Chars)==8:
+			charList = list(Chars)
+			for i in range(numChars-1):
+				newList.append(charList[i+1])
+			newList.append(Char)
+			return(''.join(newList))
+		else:
+			charList = list(Chars)
+			charList.append(Char)
+			return(''.join(charList))
+	elif numChars==9:
+		if len(Chars)==9:
+			charList = list(Chars)
+			for i in range(numChars-1):
+				newList.append(charList[i+1])
+			newList.append(Char)
+			return(''.join(newList))
+		else:
+			charList = list(Chars)
+			charList.append(Char)
+		return(''.join(charList))
+
+def main():
+	r = requests.get('http://streamerapi.finance.yahoo.com/streamer/1.0?s=MSFT&k=l84&callback=parent.yfs_u1f&mktmcb=parent.yfs_mktmcb&gencallback=parent.yfs_gencb', stream=True)
+
+	tagB = ''
+	tagE = ''
+	count =0
+	beginQ = '<script>'
+	endQ = '</script>'
+	inState = False
+	dataCollect = ''
+
+
+	for char in r.iter_content():
+		c = char.decode()
+		tagB = stringGen(tagB,c,8)
+		tagE = stringGen(tagE,c,9)
+		
+		print(tagB)
+		print(tagE)
+
+		if tagB == beginQ:
+			print('___',tagB)
+			inState = True
+		if tagE == endQ:
+			print(dataCollect[:len(dataCollect)-8])
+			dataCollect = ''
+			inState = False
+
+		if inState == True:
+			dataCollect = dataCollect + c		
+
+
+if __name__ == '__main__':
+	main()
